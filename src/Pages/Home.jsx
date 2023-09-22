ae736d03c125374841da9d9d5229fa0905087b97
RyanCornelio <ryan.cornelio@gmail.com>
2023-09-23 00:40:50 +0200

    save point

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,31 @@ const Home = () => {
     return (
         <>
             <div className="App">
+                <div className="box">
+                    <section className='container'>
+                        <img src="/public/logo.png" alt="" className='logotype' />
+                    </section>
+                </div>
+
                 <div className="container center home-page">
                     <h1>
                         Varmt Välkomna
                     </h1>
                 </div>
+                <div className="container">
+                    <section className='second-part'>
+                        <img src="/src/assets/images/12.JPG" alt="" className='image' />
+                        <img src="/src/assets/images/22.JPG" alt="" className='image' />
+                        <img src="/src/assets/images/11.JPG" alt="" className='image' />
+                    </section>
+                </div>
+                <div className="container">
+                    <section className="third-part box">
+                        <h1>Tai Lee är en del av Kungälv.</h1>
+                    </section>
+                </div>
             </div>
+
 
         </>
     )
