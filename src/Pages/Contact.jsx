7a0ec750dc6951b29c5b46a0791ca2153bfc7791
RyanCornelio <ryan.cornelio@gmail.com>
2023-09-22 10:39:07 +0200

    checkpoint

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
         <>
             <div className='container box center contact'>
-                <h1>Telefon: 0303-18171</h1>
+                <h1>Telefon: 0303-18177</h1>
                 <h4>Nytorget 1, Kungälv</h4>
             </div>
 
--- src/Pages/TakeAway.jsx
+++ src/Pages/TakeAway.jsx
@@ -4,76 +4,111 @@ import '../Styles/main.css'
 const TakeAway = () => {
     return (
         <>
-            <div className='container main-container'></div>
-            <div className="box container">
+            <div className='container main-container'>
+                <div className="box box-2">
 
-                <div className='container'>
-                    <h4>1. Kinesiska vårrullar 76:- </h4>
-                    <p>2 st vårrullar med köttfärs och vitkål.</p>
-                </div>
-                <div className="container">
-                    <h4>2. Vegetariska vårrullar 68:-</h4>
-                    <p>10 st små vårrullar</p>
-                </div>
-                <div className="container">
-                    <h4>3. Thai Basilika, stark! 120:-</h4>
+                    <div className='container'>
+                        <h4>1. Kinesiska vårrullar 76:- </h4>
+                        <p>2 st vårrullar med köttfärs och vitkål.</p>
+                    </div>
+                    <div className="container">
+                        <h4>2. Vegetariska vårrullar 68:-</h4>
+                        <p>10 st små vårrullar</p>
+                    </div>
+                    <div className="container">
+                        <h4>3. Thai Basilika, stark! 120:-</h4>
 
-                    <p>(Innehåller vetemjöl) Kycklingfilé med stark thai basilika, lök & purjolök (biff 133:- /räkor 143:-)</p>
+                        <p>(Innehåller vetemjöl) Kycklingfilé med stark thai basilika, lök & purjolök (biff 133:- /räkor 143:-)</p>
+                    </div>
+                    <div className="container">
+                        <h4>4. Sambal Indo Wok, lite stark! 120:-</h4>
+                        <p>(Innehåller mjölk) Wokad Malaysia stark chilisås med kycklingfilé & kokosmjölk</p>
+                    </div>
+                    <div className="container">
+                        <h4>5. Szechuan Wok - stark! 123:-</h4>
+                        <p>(räkor 143:-/kycklingfilé 123:-/bläckfisk 123:-) Wokad biff i kinesisk stark sås</p>
+                    </div>
+                    <div className="container">
+                        <h4>6. Chin Chong 133:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad biff med a: lök b: purjolök c: paprika</p>
+                    </div>
+                    <div className="container">
+                        <h4>7. Malaysia Curry 128:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad kycklingfilé med grönsaker i curry (biff 133:- /räkor 143:-)</p>
+                    </div>
+                    <div className="container">
+                        <h4>8. Shanghai Wok 133:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad biff med a: bambuskott b: champinjoner</p>
+                    </div>
+                    <div className="container">
+                        <h4>9. Best of the best 120:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad kycklingfilé med grönsaker och cashewnötter (räkor 143:-)</p>
+                    </div>
+                    <div className="container">
+                        <h4>10. Hot Garlic Chicken 118:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad kycklingfilé i stark vitlökssås</p>
+                    </div>
+                    <div className="container">
+                        <h4>11. Chop Suey Deluxe 138:-</h4>
+                        <p>(Innehåller vetemjöl) Wokade grönsaker med kycklingfilé, räkor & bläckfisk i kinesisk stil</p>
+                    </div>
+                    <div className="container">
+                        <h4>12. Chow Sam Sing - Lite stark! 148:-</h4>
+                        <p>Wokade grönsaker med kycklingfilé & räkor i stark sötsursås & vitlök</p>
+                    </div>
+                    <div className="container">
+                        <h4>13. Red Hot Phuket 138:-</h4>
+                        <p>Wokade grönsaker med kycklingfilé & räkor i stark thai röd curry & kokosmjölk</p>
+                    </div>
                 </div>
-                <div className="container">
-                    <h4>4. Sambal Indo Wok, lite stark! 120:-</h4>
-                    <p>(Innehåller mjölk) Wokad Malaysia stark chilisås med kycklingfilé & kokosmjölk)</p>
-                </div>
-                <div className="container">
-                    <h4>5. Szechuan Wok - stark! 123:-</h4>
-                    <p>(räkor 143:-/kycklingfilé 123:-/bläckfisk 123:-) Wokad biff i kinesisk stark sås</p>
-                </div>
-                <div className="container">
-                    <h4>6. Chin Chong 133:-</h4>
-                    <p>(Innehåller vetemjöl) Wokad biff med a) lök b) purjolök c) paprika</p>
-                </div>
-                <div className="container">
-                    <h4>7. Malaysia Curry 128:-</h4>
-                    <p>(Innehåller vetemjöl) Wokad kycklingfilé med grönsaker i curry (biff 133:- /räkor 143:-</p>
-                </div>
-                <div className="container">
-                    <h4>8. Shanghai Wok 133:-</h4>
-                    <p>(Innehåller vetemjöl) Wokad biff med a) bambuskott b) champinjoner</p>
-                </div>
-                <div className="container">
-                    <h4>9. Best of the best 120:-</h4>
-                    <p>(Innehåller vetemjöl) Wokad kycklingfilé med grönsaker och cashewnötter (räkor 143:-)</p>
-                </div>
-                <div className="container">
-                    <h4>10. Hot Garlic Chicken 118:-</h4>
-                    <p>(Innehåller vetemjöl) Wokad kycklingfilé i stark vitlökssås</p>
-                </div>
-                <div className="container">
-                    <h4>11. Chop Suey Deluxe 138:-</h4>
-                    <p>(Innehåller vetemjöl) Wokade grönsaker med kycklingfilé, räkor & bläckfisk i kinesisk stil</p>
-                </div>
-                <div className="container">
-                    <h4>12. Chow Sam Sing - Lite stark! 148:-</h4>
-                    <p>Wokade grönsaker med kycklingfilé & räkor i stark sötsursås & vitlök</p>
-                </div>
-                <div className="container">
-                    <h4>13. Red Hot Phuket 138:-</h4>
-                    <p>Wokade grönsaker med kycklingfilé & räkor i stark thai röd curry & kokosmjölk</p>
-                </div>
-            </div>
-            <div className="box box-2">
+                <div className="box box-2">
 
-                <div className="container">
-                    <h4>14. Forest Thai - Mycket stark! 138:-</h4>
-                    <p>Wokad biff i stark thai gröncurry & kokosmjölk</p>
-                </div>
-                <div className="container">
-                    <h4>14. Forest Thai - Mycket stark! 138:-</h4>
-                    <p>Wokad biff i stark thai gröncurry & kokosmjölk</p>
-                </div>
-                <div className="container">
-                    <h4>14. Forest Thai - Mycket stark! 138:-</h4>
-                    <p>Wokad biff i stark thai gröncurry & kokosmjölk</p>
+                    <div className="container">
+                        <h4>14. Forest Thai - Mycket stark! 138:-</h4>
+                        <p>Wokad biff i stark thai gröncurry & kokosmjölk</p>
+                    </div>
+                    <div className="container">
+                        <h4>15. Hot Wok mix 138:-</h4>
+                        <p>(Innehåller vetemjöl, sesampasta) Wokad kycklingfilé & räkor med grönsaker i hoisin sås eller ostron sås
+                        </p>
+                    </div>
+                    <div className="container">
+                        <h4>16. Mr.Bean 128:-</h4>
+                        <p>(Innehåller vetemjöl) Wokad biff i starka svarta bönor & paprika</p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div className="container">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+
                 </div>
             </div>
         </>
