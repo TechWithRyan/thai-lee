922f827550b640fe4b9bd92871b4b38dd0ec1c7f
RyanCornelio <ryan.cornelio@gmail.com>
2023-09-24 18:24:47 +0200

    pushing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../Styles/main.css";
-
+import banner from '../assets/55.jpg';
 import image0 from '../assets/50.jpg';
 import image1 from '../assets/51.jpg';
 import image2 from '../assets/52.jpg';
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="App">
                 <div className="container-text">
                     <section className='text-over'>
-                        <img src={image0} alt="" className='banner' />
+                        <img src={banner} alt="" className='banner' />
 
                     </section>
                     <h1 className='centered'>Restaurang Tai Lee</h1>
@@ -31,6 +31,7 @@ const Home = () => {
                 <div className="container images">
                     <img src={image4} alt="" className='image' />
                     <img src={image3} alt="" className='image' />
+                    <img src={banner} alt="" className='image' />
                     <img src={image0} alt="" className='image' />
                     <img src={image1} alt="" className='image' />
                     <img src={image2} alt="" className='image' />
@@ -38,6 +39,8 @@ const Home = () => {
                 <div className="container third-parts">
                     <p>På Tai Lee serverar vi en mångfald av rätter från olika asiatiska kök. Våra erfarna kockar använder endast färska och högkvalitativa ingredienser för att skapa smakfulla och autentiska rätter.</p>
                 </div>
+
+
             </div >
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import banner from '../assets/55.jpg';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    image(), // lägg till den här
+
+  ],
 
   build: {
     rollupOptions: {
@@ -12,8 +17,5 @@ export default defineConfig({
     },
   },
 
-
   assetsInclude: /\.(png|jpg|jpeg|gif|svg)$/,
-
-
 });
