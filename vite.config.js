38a5b44f279f8b6fac4080d763e23de4218139b2
RyanCornelio <ryan.cornelio@gmail.com>
2023-09-24 18:29:24 +0200

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import banner from '../assets/55.jpg';
+import image6 from '../assets/55.jpg';
 
 export default defineConfig({
   plugins: [
     react(),
-    image(), // lägg till den här
+    image6(), // lägg till den här
 
   ],
 
