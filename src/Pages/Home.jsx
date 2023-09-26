726b6ff8f8963f6af3af40a2339829398946ff95
RyanCornelio <ryan.cornelio@gmail.com>
2023-09-26 13:58:22 +0200

    mobile responsive and layout fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import "../Styles/main.css"
+import photo1 from "../assets/images/55.JPG";
+import photo2 from "../assets/images/54.JPG";
+import photo3 from "../assets/images/53.JPG";
+import photo4 from "../assets/images/52.JPG";
+import photo5 from "../assets/images/51.JPG";
+import photo6 from "../assets/images/50.JPG";
+
+
 
 const Home = () => {
     return (
@@ -7,17 +15,17 @@ const Home = () => {
             <div className="App">
                 <div className="container-text">
                     <section className='text-over'>
-                        <img src="/src/assets/images/55.JPG" alt="" className='banner' />
+                        <img src={photo1} alt="" className='banner' />
 
                     </section>
-                    <h1 className='centered'>Restaurang Tai Lee</h1>
+                    <div className='centered'>Restaurang Tai Lee</div>
                 </div>
 
-                <div className="container center home-page">
+                <div className="container home-page">
 
 
-                    <h1>Vi hälsar dig varmt välkommen till Tai Lee</h1>
-                    <p>
+                    <div className='welcome-text'>Vi hälsar dig varmt välkommen till Tai Lee</div>
+                    <p >
                         Din destination för en
                         autentisk asiatisk matupplevelse i hjärtat av Kungälv.
                         Vår restaurang tar dig med på en kulinarisk resa genom Asiens rika smaker
@@ -25,16 +33,20 @@ const Home = () => {
                     </p>
                 </div>
                 <div className="container">
-                    <section className='second-part'>
-                        <img src="/src/assets/images/12.JPG" alt="" className='image' />
-                        <img src="/src/assets/images/22.JPG" alt="" className='image' />
-                        <img src="/src/assets/images/11.JPG" alt="" className='image' />
-                    </section>
+                    <div className='second-part'>
+                        <img src={photo1} alt="" className='image' />
+                        <img src={photo2} alt="" className='image' />
+                        <img src={photo3} alt="" className='image' />
+                        <img src={photo4} alt="" className='image' />
+                        <img src={photo5} alt="" className='image' />
+                        <img src={photo6} alt="" className='image' />
+
+                    </div>
                 </div>
                 <div className="container">
-                    <section className="third-part center">
-                        <h1>På Tai Lee serverar vi en mångfald av rätter från olika asiatiska kök. Våra erfarna kockar använder endast färska och högkvalitativa ingredienser för att skapa smakfulla och autentiska rätter.</h1>
-                    </section>
+                    <div className="third-part center">
+                        <div className='dish-text'>På Tai Lee serverar vi en mångfald av rätter från olika asiatiska kök. Våra erfarna kockar använder endast färska och högkvalitativa ingredienser för att skapa smakfulla och autentiska rätter.</div>
+                    </div>
                 </div>
             </div >
 
